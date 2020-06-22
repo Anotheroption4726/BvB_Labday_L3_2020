06fa69f95194dc5dd3aa97159ba81f921ec0af98
Felix <felix.litou@gmail.com>
2020-06-22 16:59:01 +0200

    Scripts

---
+++ Assets/Scripts/MenuScripts/LoginToSignupScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginToSignupScript : MonoBehaviour
+{
+    public void NavigationSignup()
+    {
+        SceneManager.LoadScene("NouveauCompte", LoadSceneMode.Single);
+    }
+}
