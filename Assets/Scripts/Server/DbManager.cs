47e7081542e723fdf546e78093ffdb3894415cd5
Felix <felix.litou@gmail.com>
2020-06-22 18:49:59 +0200

    User methods

--- Assets/Scripts/Server/DbManager.cs
+++ Assets/Scripts/Server/DbManager.cs
@@ -2,6 +2,10 @@
 
 public static class DbManager 
 {
-
+    /*static public User_Player Server_UserToUser(Server_User arg_serverUser)
+    {
+        User loc_user = new User_Player(1, AccountTypeEnum.Player, "token", );
+        return loc_user;
+    }*/
 
 }
