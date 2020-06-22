9db83d0e6a469a6b8186ad776a29133b6f50a418
Adrien <napalm_ground@hotmail.com>
2020-06-22 12:58:29 +0200

    Server_Database getters & setters

--- Assets/Scripts/Server/Server_Database.cs
+++ Assets/Scripts/Server/Server_Database.cs
@@ -6,7 +6,7 @@ public static class Server_Database
 	//	email
 	//	password
 	//	accountType
-	static Server_User[] usersTable = { new Server_User(1, "testemail@bvb.com", "testpassword", AccountTypeEnum.Player) };
+	private static Server_User[] usersTable = { new Server_User(1, "testemail@bvb.com", "testpassword", AccountTypeEnum.Player) };
 
 	//	userId
 	//	weaponId
@@ -16,7 +16,7 @@ public static class Server_Database
 	//	behaviorProximity
 	//	behaviorAgility
 	//	behaviorAggressivity
-	static Server_Robot_Player[] playerRobotsTable = { new Server_Robot_Player(1, 1, 30, 50, 40, 0, 40, 50) };
+	private static Server_Robot_Player[] playerRobotsTable = { new Server_Robot_Player(1, 1, 30, 50, 40, 0, 40, 50) };
 
 	//	algoGenId
 	//	weaponId
@@ -26,7 +26,7 @@ public static class Server_Database
 	//	behaviorProximity
 	//	behaviorAgility
 	//	behaviorAggressivity
-	static Server_Robot_AlgoGen[] algoGenRobotsTable = { new Server_Robot_AlgoGen(1, 2, 10, 70, 20, 0, 20, 10) };
+	private static Server_Robot_AlgoGen[] algoGenRobotsTable = { new Server_Robot_AlgoGen(1, 2, 10, 70, 20, 0, 20, 10) };
 
 	//	id
 	//	bulletId
@@ -35,5 +35,27 @@ public static class Server_Database
 	//	minRange
 	//	rateOfFire
 	//	damageValue
-	static Server_Weapon[] weaponsTable = { new Server_Weapon(1, 1, 4, 8, 4, 4, 2), new Server_Weapon(2, 1, 2, 14, 8, 1, 4) };
+	private static Server_Weapon[] weaponsTable = { new Server_Weapon(1, 1, 4, 8, 4, 4, 2), new Server_Weapon(2, 1, 2, 14, 8, 1, 4) };
+
+
+	//	Getters
+	public static Server_User[] GetUsersTable()
+	{
+		return usersTable;
+	}
+
+	public static Server_Robot_Player[] GetPlayerRobotsTable()
+	{
+		return playerRobotsTable;
+	}
+
+	public static Server_Robot_AlgoGen[] GetAlgoGenRobotsTable()
+	{
+		return algoGenRobotsTable;
+	}
+
+	public static Server_Weapon[] GetWeaponsTable()
+	{
+		return weaponsTable;
+	}
 }
