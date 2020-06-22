bffdc32418159b799d684b1a527b9c1c78d07d81
Adrien <napalm_ground@hotmail.com>
2020-06-22 11:59:39 +0200

    User_Player class

--- Assets/Scripts/Classes/User_Player.cs
+++ Assets/Scripts/Classes/User_Player.cs
@@ -2,19 +2,38 @@
 
 public class User_Player
 {
-	//	private int wins;
-	//	private int losses;
-	//	NO need win/losses here? Update server at the end of each match
-
 	private Robot_Player userRobot;
 	private Robot_AlgoGen enemyRobot;
 
 
 	// Constructor
+	public User_Player(Robot_Player arg_userRobot, Robot_AlgoGen arg_enemyRobot)
+	{
+		userRobot = arg_userRobot;
+		enemyRobot = arg_enemyRobot;
+	}
 
 
 	// Getters
+	public Robot_Player GetUserRobot()
+	{
+		return userRobot;
+	}
+
+	public Robot_AlgoGen GetEnemyRobot()
+	{
+		return enemyRobot;
+	}
 
 
 	// Setters
+	public void SetUserRobot(Robot_Player arg_userRobot)
+	{
+		userRobot = arg_userRobot;
+	}
+
+	public void SetEnemyRobot(Robot_AlgoGen arg_enemyRobot)
+	{
+		enemyRobot = arg_enemyRobot;
+	}
 }
