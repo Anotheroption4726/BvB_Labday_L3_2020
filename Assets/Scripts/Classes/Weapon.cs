943273d58800e72f5098d753fb3b486bd22d7b94
Adrien <napalm_ground@hotmail.com>
2020-06-22 11:41:36 +0200

    Weapon class

--- Assets/Scripts/Classes/Weapon.cs
+++ Assets/Scripts/Classes/Weapon.cs
@@ -2,7 +2,7 @@
 
 public class Weapon
 {
-	private int bulletId;
+	private Bullet bullet;
 	private int bulletSpeed;
 	private int maxRange;
 	private int minRange;
@@ -11,11 +11,45 @@ public class Weapon
 	
 
 	//	Constructor
-	public Weapon()
+	public Weapon(Bullet arg_bullet, int arg_bulletSpeed, int arg_maxRange, int arg_minRange, int arg_rateOfFire, int arg_damageValue)
 	{
-
+		bullet = arg_bullet;
+		bulletSpeed = arg_bulletSpeed;
+		maxRange = arg_maxRange;
+		minRange = arg_minRange;
+		rateOfFire = arg_rateOfFire;
+		damageValue = arg_damageValue;
 	}
 
 
 	// Getters
+	public Bullet GetBulletId()
+	{
+		return bullet;
+	}
+
+	public int GetBulletSpeed()
+	{
+		return bulletSpeed;
+	}
+
+	public int GetMaxRange()
+	{
+		return maxRange;
+	}
+
+	public int GetMinRange()
+	{
+		return minRange;
+	}
+
+	public int GetRateOfFire()
+	{
+		return rateOfFire;
+	}
+
+	public int GetDamageValue()
+	{
+		return damageValue;
+	}
 }
