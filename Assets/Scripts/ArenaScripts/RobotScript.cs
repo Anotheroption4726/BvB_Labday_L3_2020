4ea36feb876be5252bb7856546087a2efafc4855
Adrien <napalm_ground@hotmail.com>
2020-06-23 14:59:46 +0200

    Robot movement + dodging

--- Assets/Scripts/ArenaScripts/RobotScript.cs
+++ Assets/Scripts/ArenaScripts/RobotScript.cs
@@ -126,24 +126,29 @@ public class RobotScript : MonoBehaviour
         robotRigidbody = gameObject.GetComponent<Rigidbody>();
     }
 
-    
+
     private void FixedUpdate()
     {
         if (Session.GetGameState().Equals(GameStateEnum.GameStarted))
         {
+            if (robotRigidbody.velocity.magnitude > 10)
+            {
+                //  NormalizeRigidBodyVelocity();
+            }
+
             RobotAim(transform, enemyRobotGameObject.transform.position);
 
             if (RobotCheckAngle(transform, enemyRobotGameObject.transform.position))
             {
                 if (RobotCheckRange(transform, enemyRobotGameObject.transform.position) >= robot.GetWeapon().GetMaxRange())
                 {
-                    //RobotMove(1, robotRigidbody);
-                    //RobotDodge(transform, robotRigidbody);
+                    RobotMove(1, robotRigidbody);
+                    RobotDodge(transform, robotRigidbody);
                 }
                 else if (RobotCheckRange(transform, enemyRobotGameObject.transform.position) <= robot.GetWeapon().GetMinRange())
                 {
-                    //RobotMove(-1, robotRigidbody);
-                    //RobotDodge(transform, robotRigidbody);
+                    RobotMove(-1, robotRigidbody);
+                    RobotDodge(transform, robotRigidbody);
                 }
                 else
                 {
@@ -188,5 +193,75 @@ public class RobotScript : MonoBehaviour
     {
         float loc_distance = Vector3.Distance(arg_enemyTransformPosition, arg_robotTransform.position);
         return loc_distance;
+    }
+
+
+    //  Méthode de déplacement
+    private void RobotMove(int arg_direction, Rigidbody arg_robotRigidbody)
+    {
+        double loc_robotSpeedDouble = Convert.ToSingle(arg_direction * robot.GetStatSpeed() / 2);
+        loc_robotSpeedDouble = Math.Round(loc_robotSpeedDouble, 2);
+        float loc_robotSpeedFloat = (float)loc_robotSpeedDouble;
+
+        if (robot.GetStatSpeed() >= 0)
+        {
+            arg_robotRigidbody.AddForce(arg_robotRigidbody.transform.TransformDirection(new Vector3(0.0f, 0.0f, loc_robotSpeedFloat)));
+        }
+        else
+        {
+            arg_robotRigidbody.AddForce(arg_robotRigidbody.transform.TransformDirection(new Vector3(0.0f, 0.0f, 0.75f * loc_robotSpeedFloat)));
+        }
+    }
+
+
+    //  Méthode d'Esquive
+    private void RobotDodge(Transform arg_robotTransform, Rigidbody arg_robotRigidbody)
+    {
+        int loc_randomNumber = UnityEngine.Random.Range(0, 100);
+        float loc_timer = 0.0f;
+        float loc_runTime = 0.1f;
+        int loc_dodgeDirectionRight = 0;
+        float loc_dodgeFrequency = Convert.ToSingle(robot.GetBehaviorAgility()) / 10;
+        float loc_dodgeSpeed = Convert.ToSingle(robot.GetStatSpeed()) * 8;
+
+        RaycastHit loc_raycastHit;
+
+        //Debug.DrawRay(transform.position, transform.right * 3, Color.red);
+        //Debug.DrawRay(transform.position, -transform.right * 3, Color.red);
+
+        if ((loc_randomNumber % 2) == 0)
+        {
+            if (!Physics.Raycast(arg_robotTransform.position, arg_robotTransform.right, out loc_raycastHit, 3))
+            {
+                loc_dodgeDirectionRight = 1;
+            }
+            else
+            {
+                loc_dodgeDirectionRight = -1;
+            }
+
+        }
+        else if ((loc_randomNumber % 2) != 0)
+        {
+            if (!Physics.Raycast(arg_robotTransform.position, -arg_robotTransform.right, out loc_raycastHit, 3))
+            {
+                loc_dodgeDirectionRight = -1;
+            }
+            else
+            {
+                loc_dodgeDirectionRight = 1;
+            }
+        }
+
+        if (loc_randomNumber + (loc_dodgeFrequency) > 100)
+        {
+            while (loc_timer < loc_runTime)
+            {
+                arg_robotRigidbody.AddForce(arg_robotRigidbody.transform.TransformDirection(new Vector3(loc_dodgeDirectionRight * loc_dodgeSpeed, 0.0f, 0.0f)));
+                loc_timer += Time.deltaTime;
+            }
+
+            arg_robotRigidbody.velocity = new Vector3(0, 0, arg_robotRigidbody.velocity.z);
+        }
     }
 }
