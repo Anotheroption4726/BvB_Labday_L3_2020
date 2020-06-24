a65a0da813c8762065b319478e5bc37a32b91b9d
Felix <felix.litou@gmail.com>
2020-06-24 15:45:19 +0200

    Navigation Menu

---
+++ Assets/Scripts/MenuScripts/StatRobotToMenuScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StatRobotToMenuScript : MonoBehaviour
+{
+    public void NavigationMenu()
+    {
+        SceneManager.LoadScene("MenuMainScene", LoadSceneMode.Single);
+    }
+}
