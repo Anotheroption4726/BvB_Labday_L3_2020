ca0684bbab5dccf3b0a9b52f20d34e1dd94b7532
Adrien <napalm_ground@hotmail.com>
2020-06-26 11:41:00 +0200

    github button

---
+++ Assets/Scripts/MenuScripts/GithubButtonScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GithubButtonScript : MonoBehaviour
+{
+    public void OpenURL()
+    {
+        Application.OpenURL("https://github.com/Mouthissimo/BvB_Labday_L3_2020");
+    }
+}
